dd662e51fba8f24a52607dc4787630f00be2479d
Shubh-Sharma <shubham.sharma@lendbox.in>
2021-11-29 19:35:20 +0530

    feat: event bus based dialog component in portal

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,27 +3,22 @@ export default {
   head: {
     title: 'Per-Annum',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-'~/assets/style/style.css'
-  ],
+  css: ['~/assets/style/style.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -35,30 +30,30 @@ export default {
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
     'nuxt-purgecss',
-    '@nuxtjs/composition-api/module'
+    '@nuxtjs/composition-api/module',
   ],
   purgeCSS: {
     // your settings here
-  mode: 'webpack',
-  // enabled: ({ isDev, isClient }) => (isDev && isClient), // or `false` when in dev/debug mode
-  enabled:true,
-  paths: [
-    'components/*.vue',
-   // 'layouts/**/*.vue',
-    'pages/*.vue',
-    // 'plugins/**/*.js',
-    'assets/**/*.css'
-  ],
-  css: ['assets/style/style.css'],
-  styleExtensions: ['.css'],
-  // whitelist: ['body', 'html', 'nuxt-progress'],
-  extractors: [
-    {
-      extractor: content => content.match(/[A-z0-9-:\\/]+/g) || [],
-      extensions: ['html', 'vue', 'js']
-    }
-  ]
-   },
+    mode: 'webpack',
+    // enabled: ({ isDev, isClient }) => (isDev && isClient), // or `false` when in dev/debug mode
+    enabled: true,
+    paths: [
+      'components/*.vue',
+      // 'layouts/**/*.vue',
+      'pages/*.vue',
+      // 'plugins/**/*.js',
+      'assets/**/*.css',
+    ],
+    css: ['assets/style/style.css'],
+    styleExtensions: ['.css'],
+    // whitelist: ['body', 'html', 'nuxt-progress'],
+    extractors: [
+      {
+        extractor: (content) => content.match(/[A-z0-9-:\\/]+/g) || [],
+        extensions: ['html', 'vue', 'js'],
+      },
+    ],
+  },
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     'portal-vue/nuxt',
@@ -68,12 +63,12 @@ export default {
         locales: [
           {
             code: 'en',
-            file: 'en.json'
+            file: 'en.json',
           },
           {
             code: 'hi',
-            file: 'hi.json'
-          }
+            file: 'hi.json',
+          },
         ],
         lazy: true,
         langDir: 'locales/',
@@ -81,16 +76,16 @@ export default {
         defaultLocale: 'en',
         detectBrowserLanguage: {
           useCookie: true,
-          cookieKey: 'i18n_redirected'
+          cookieKey: 'i18n_redirected',
           // onlyOnRoot: true
-        }
-      }
-    ]
+        },
+      },
+    ],
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     extractCSS: true,
-    analyze: true
-  }
+    analyze: true,
+  },
 }
